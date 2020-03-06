8c9c59da569510eea9d1557f3b2eb3b0d063f4b5
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-03-06 13:16:01 +0100

    added paddle.cs

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthUnits = 16f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosUnits = Input.mousePosition.x/ Screen.width * screenWidthUnits;
+        Vector2 paddlePos = new Vector2(mousePosUnits, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
