0814b104f07453b147703b25db3f8b0e45008581
NancySchmeisser <nancy.schmeisser@netzalist.de>
2020-03-10 14:18:17 +0100

    added screen.width, Mathf.Clamp

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,20 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    [SerializeField] float screenWidthUnits = 16f;
+    // [SerializeField] float screen.width = 1215;
+    [SerializeField] float maxX = 1215;
+    [SerializeField] float minX = 0;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        var sp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var paddlePosOnScreen = new Vector2(Mathf.Clamp(Input.mousePosition.x, 0, Screen.width), Input.mousePosition.y);
+        var sp = Camera.main.ScreenToWorldPoint(paddlePosOnScreen); 
         Vector2 paddlePos = new Vector2(sp.x, transform.position.y);
         transform.position = paddlePos;
+        paddlePos.x = Mathf.Clamp(sp.x, minX, maxX);
+
     }
 }
+
+  
